ef221e613c340654ab742b011d9001a8ea53bcbb
Steve Kane <kanesteven@gmail.com>
2015-04-13 17:00:34 -0500

    updated the scripts to use webcam and check if status is ready before sending texture data to GPU

--- src/gpu-particles.js
+++ src/gpu-particles.js
@@ -9,6 +9,7 @@ var emitter           = new GPUEmitter(shell.gl, 0, 0, 0, vidTexture)
 var gpuParticleSystem = new GPUParticleSystem(shell.gl)
 var emitters          = [emitter]
 var videoEl           = document.getElementById("video")
+var getUserMedia      = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia
 
 videoEl.addEventListener("canplaythrough", function () {
   videoEl.play()
@@ -18,18 +19,32 @@ videoEl.addEventListener("ended", function () {
   console.log("done")
 }, true)
 
-videoEl.preload = true
-videoEl.src     = "small.mp4"
+function withCam (stream) {
+  videoEl.src = window.URL.createObjectURL(stream)
+  videoEl.onloadedmetadata = function () {
+    videoEl.play() 
+  }
+}
+
+function withoutCam () {
+  console.log("we couldn't get access to your webcam")
+}
+
+getUserMedia.call(navigator, {audio: true, video: true}, withCam, withoutCam)
+
 
 shell.render = function () {
   var gl = this.gl
 
-  gl.activeTexture(gl.TEXTURE0 + 10)
-  gl.bindTexture(gl.TEXTURE_2D, emitter.sourceTexture)
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
-                gl.UNSIGNED_BYTE, videoEl)
-  gpuParticleSystem.render(emitters)
+  //just a quick hack to ensure camera is ready
+  if (videoEl.readyState === 4) {
+    gl.activeTexture(gl.TEXTURE0 + 10)
+    gl.bindTexture(gl.TEXTURE_2D, emitter.sourceTexture)
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+                  gl.UNSIGNED_BYTE, videoEl)
+    gpuParticleSystem.render(emitters)
+  }
 }
 
 shell.update = function (dT) {
